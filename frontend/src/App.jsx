8e3174cdbc3c0a457085ce64471b5b7478db3807
tanushrithakare <tanushrithakare472@gmail.com>
2025-10-02 00:45:00 +0530

    # cyber sec add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>🎤 Aavaj-Adhikar</h1>
-        <p>Voice Rights Platform - Frontend Development</p>
-      </header>
+    <div className="page">
+      <h1>AAVAJ &amp; ADHIKAR</h1>
+      <p>Empowering Citizens with Legal Awareness &amp; Government Schemes</p>
+      <p>
+        One platform for all welfare schemes, legal rights, and support in simple
+        language
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/vite.config.js
@@ -0,0 +1,6 @@
+export default {
+  server: {
+    port: 5173,
+    host: true
+  }
+}
